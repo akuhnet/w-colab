2311e468cb9a230c4d0f1b64860c8a28298f64dc
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-10 01:11:41 +0700

    Update w7.sh

--- w7.sh
+++ w7.sh
@@ -1,6 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
+read -p "Paste authtoken here: " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait installing...
