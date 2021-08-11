7bf30478b080b8fda548183605e7ed539a5f5d2d
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-11 11:46:33 +0700

    Update akuhnet-w10.sh

--- akuhnet-w10.sh
+++ akuhnet-w10.sh
@@ -7,12 +7,12 @@ nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait installing...
 wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
 unzip 1.zip > /dev/null 2>&1
-wget https://bit.ly/3AtMURb -O rootfs.tar.xz > /dev/null 2>&1
+wget hhttps://transfer.sh/12uZFqZ/rootfs.tar.xz > /dev/null 2>&1
 tar -xvf rootfs.tar.xz > /dev/null 2>&1
 echo "Wait 1 minute"
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Download Windows10
-wget https://bit.ly/akuhnetw10 -O w10.qcow2
+wget https://transfer.sh/1AaMJso/w10.qcow2-O w10.qcow2
 echo "Windows 10 Google Colab"
 echo Your VNC Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
