3e1d1bb525c78e4bfe47b2d032d9637199a6bcf7
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-11 11:16:13 +0700

    Update akuhnet-w7.sh

--- akuhnet-w7.sh
+++ akuhnet-w7.sh
@@ -6,12 +6,12 @@ nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait installing...
 wget https://bit.ly/3AtMURb -O rootfs.tar.xz > /dev/null 2>&1
 unzip 1.zip > /dev/null 2>&1
-wget https://bit.ly/3s5vY0i -O rootfs.tar.xz > /dev/null 2>&1
+wget hhttps://transfer.sh/12uZFqZ/rootfs.tar.xz > /dev/null 2>&1
 tar -xvf rootfs.tar.xz > /dev/null 2>&1
 echo "Wait 1 minute"
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Download Windows7
-wget https://bit.ly/akuhnetw7 -O w7.qcow2
+wget https://transfer.sh/w7.qcow2 -O w7.qcow2
 echo "Windows 7 Google Colab"
 echo Your VNC Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
