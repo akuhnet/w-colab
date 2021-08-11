6a19bdd012dfc57c80d2e8b1f78c76424901a55e
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-11 22:27:38 +0700

    Update akuhnet-w7.sh

--- akuhnet-w7.sh
+++ akuhnet-w7.sh
@@ -8,7 +8,7 @@ wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
 unzip 1.zip > /dev/null 2>&1
 wget https://transfer.sh/12uZFqZ/rootfs.tar.xz > /dev/null 2>&1
 tar -xvf rootfs.tar.xz > /dev/null 2>&1
-echo "Wait 1 minute"
+echo "Wait 5 minute"
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Download Windows7
 wget https://transfer.sh/1yr7AGF/w7.qcow2 -O w7.qcow2
@@ -17,4 +17,5 @@ echo Your VNC Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Ctrl+C To Copy"
 echo "Keep Colab Tab Open, Maximum Time 12h"
+echo "Wait 1 minute to complete startup "
 ./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda w7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
