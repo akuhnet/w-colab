22a9f863b8985bd5ce038746390f0b7f22f77ef1
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-11 09:25:37 +0700

    Update akuhnet-wxp.sh

--- akuhnet-wxp.sh
+++ akuhnet-wxp.sh
@@ -17,5 +17,4 @@ echo Your VNC Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Ctrl+C To Copy"
 echo "Keep Colab Tab Open, Maximum Time 12h"
-echo "If this works please give akuh.net support by clicking 1 ad okay bro, Thank you very much"
 ./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda wxp.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
