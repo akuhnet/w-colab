0905da90cc78c690e14a830211bad8e8955b2057
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-14 12:00:08 +0700

    Update w7x64.sh

--- w7x64.sh
+++ w7x64.sh
@@ -1,19 +1,20 @@
+echo "Download windows files"
+wget -O w7x86.img https://bit.ly/akuhnetw7x86
+echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP
 ./ngrok authtoken $CRP 
-nohup ./ngrok tcp 5900 &>/dev/null &
+nohup ./ngrok tcp 3000 &>/dev/null &
 echo Downloading File From akuh.net
-wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
-unzip 1.zip > /dev/null 2>&1
-wget https://transfer.sh/12uZFqZ/rootfs.tar.xz > /dev/null 2>&1
-tar -xvf rootfs.tar.xz > /dev/null 2>&1
-echo "Wait Installing"
-./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
-echo "Starting Windows 7 x64"
-echo VNC Address:
+apt-get install qemu
+echo "Wait"
+echo "Starting Windows"
+echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Ctrl+C To Copy"
+echo "Wait 1-2 minute to finish bot"
 echo "Dont Close This Tab"
-./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda akuhnet-w7x64.img -smp cores=4 -m 8G -machine usb=on -device usb-tablet -vga vmware > /dev/null 2>&1
+echo "Please support akuh.net thank you"
+qemu-system-x86_64 -vnc :0 -hda kali.vdi -m 8G -smp cores=2 -net user,hostfwd=tcp::3000-:4000 -net nic
 sleep 43200
