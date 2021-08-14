067832a2f2e3779aef82e360f6e993254524be85
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-14 12:26:47 +0700

    Update w7x64.sh

--- w7x64.sh
+++ w7x64.sh
@@ -4,7 +4,7 @@ echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP
-./ngrok authtoken $CRP 
+./ngrok authtoken $CRP
 nohup ./ngrok tcp 3000 &>/dev/null &
 echo Downloading File From akuh.net
 apt-get install qemu
