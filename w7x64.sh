e671024aa7d80f7f3095fb57e35bfdc7e23a9c5a
akuh <81416541+akuhnet@users.noreply.github.com>
2021-08-14 12:19:00 +0700

    Update w7x64.sh

--- w7x64.sh
+++ w7x64.sh
@@ -1,5 +1,5 @@
 echo "Download windows files"
-wget -O w7x86.img https://bit.ly/akuhnetw7x86
+wget -O w7x64.img https://bit.ly/akuhnetw7X64
 echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
@@ -16,5 +16,5 @@ echo "Ctrl+C To Copy"
 echo "Wait 1-2 minute to finish bot"
 echo "Dont Close This Tab"
 echo "Please support akuh.net thank you"
-qemu-system-x86_64 -vnc :0 -hda kali.vdi -m 8G -smp cores=2 -net user,hostfwd=tcp::3000-:4000 -net nic
+qemu-system-x86_64 -vnc :0 -hda w7x64.img -m 8G -smp cores=2 -net user,hostfwd=tcp::3000-:4000 -net nic -nographic
 sleep 43200
