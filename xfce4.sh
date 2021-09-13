898cc11a14633a332f7dba237252b2c83adef73d
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-13 18:06:52 +0700

    Update xfce4.sh

--- xfce4.sh
+++ xfce4.sh
@@ -12,10 +12,19 @@ echo "This is only for education"
 echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
 sudo apt-get update > /dev/null 2>&1
+echo "===================================="
+echo "Install Firefox"
+echo "===================================="
 sudo apt install firefox -y > /dev/null 2>&1
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
 sudo apt install -y xrdp > /dev/null 2>&1
 sudo apt install xfce4 -y > /dev/null 2>&1
 sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
 sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 echo XRDP Address:
