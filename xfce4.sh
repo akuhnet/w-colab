0c377fdfe3481e5c9ca20ba8ddf12437b9306edf
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-13 16:59:40 +0700

    Create xfce4.sh

---
+++ xfce4.sh
@@ -0,0 +1,21 @@
+echo "Download ngrok"
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp 3389 &>/dev/null &
+echo Downloading File From akuh.net
+sudo apt update && apt upgrade -y > /dev/null 2>&1
+sudo apt install firefox -y > /dev/null 2>&1
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo apt install xfce4 -y > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal -y > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo service xrdp start > /dev/null 2>&1
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "===================================="
+sleep 43200
