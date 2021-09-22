052ca6abc7a58353cc864b19e745c9d2249fec17
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 20:02:01 +0700

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,8 +1,8 @@
 echo "======================="
 echo "Download ngrok"
 echo "======================="
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok-stable-linux-amd64.zip -y > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
 read -p "Ctrl + V Authtoken: " CRP 
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region ap 3389 &>/dev/null &
