f7fb3f73bc9ee66f1fe0d20bc442cbacc619fdb6
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 23:26:00 +0700

    Update cloudshellrdp.sh

--- cloudshellrdp.sh
+++ cloudshellrdp.sh
@@ -1,3 +1,4 @@
+rm -rf ngrok  ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 echo "Download ngrok"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
