308728366afcfc43b357d7313f8e2bff5ae9129d
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 22:46:01 +0700

    Update GClxde.sh

--- GC/GClxde.sh
+++ GC/GClxde.sh
@@ -26,7 +26,9 @@ clear
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "Don't close this tab RDP runs 12 hours"
+echo "Username : root"
+echo "Password : that you make yourself"
+echo "Don't close this tab RDP runs 50 hours"
 echo "👉Keep support akuh.net thank you👈"
 echo "===================================="
 sleep 43210
