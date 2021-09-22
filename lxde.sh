07517e0fcfbd910cb2d2e1518ce0bd16b0703b24
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 20:51:52 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -5,8 +5,16 @@ echo Downloading File From akuh.net
 sudo apt-get install lxde > /dev/null 2>&1
 echo "===========40%"
 sudo apt install -y xrdp > /dev/null 2>&1
-echo "===============60%"
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "===============60%"
+echo "==============="
+echo "Install Brave"
+echo "==============="
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser > /dev/null 2>&1
 echo "===================80%"
 sudo service xrdp start > /dev/null 2>&1
 echo "=======================100%"
