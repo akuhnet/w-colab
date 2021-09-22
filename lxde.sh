1f5ff8ed58e71402b2ab516f4e6ae4a7fa888b83
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 19:49:00 +0700

    Create lxde.sh

---
+++ lxde.sh
@@ -0,0 +1,20 @@
+wget -O ngrok.sh https://bit.ly/akuhnetngrok
+chmod +x ngrok.sh
+./ng.sh
+echo Downloading File From akuh.net
+sudo apt-get install lxde
+echo "===========40%"
+sudo apt install -y xrdp
+echo "===============60%"
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "===================80%"
+sudo service xrdp start
+echo "=======================100%"
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "More  Free VPS akuh.net"
+echo "Don't close this tab RDP runs 12 hours"
+echo "Keep support akuh.net thank you"
+echo "===================================="
+sleep 43210
