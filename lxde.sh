7a2139dc782a87053b95ca0a3fa7746ec336e59a
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 19:51:02 +0700

    Update lxde.sh

--- lxde.sh
+++ lxde.sh
@@ -1,14 +1,14 @@
-wget -O ngrok.sh https://bit.ly/akuhnetngrok
+wget -O ngrok.sh https://bit.ly/akuhnetngrok > /dev/null 2>&1
 chmod +x ngrok.sh
 ./ng.sh
 echo Downloading File From akuh.net
-sudo apt-get install lxde
+sudo apt-get install lxde > /dev/null 2>&1
 echo "===========40%"
-sudo apt install -y xrdp
+sudo apt install -y xrdp > /dev/null 2>&1
 echo "===============60%"
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 echo "===================80%"
-sudo service xrdp start
+sudo service xrdp start > /dev/null 2>&1
 echo "=======================100%"
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
