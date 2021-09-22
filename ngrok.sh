33c90676bb523eb372862550d9360dccfe263eab
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 19:44:36 +0700

    Create ngrok.sh

---
+++ ngrok.sh
@@ -0,0 +1,10 @@
+echo "======================="
+echo "Download ngrok"
+echo "======================="
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip.1 > /dev/null 2>&1
+read -p "Ctrl + V Authtoken: " CRP 
+./ngrok authtoken $CRP 
+nohup ./ngrok tcp --region ap 3389 &>/dev/null &
+echo "===10 %"
