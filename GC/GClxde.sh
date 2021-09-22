d257ab259e4445bc52cd128d39e01452c558fa95
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-22 22:32:48 +0700

    Create GClxde.sh

---
+++ GC/GClxde.sh
@@ -0,0 +1,32 @@
+wget -O ng.sh https://bit.ly/akuhnetngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+echo "================================"
+echo "Wait Downloading file 3-4 minutes"
+echo "More free vps visit 👉akuh.net👈"
+echo "================================"
+sudo apt-get install -y lxde
+clear
+echo "=============40%"
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "===============60%"
+echo "==============="
+echo "Install Brave"
+echo "==============="
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser > /dev/null 2>&1
+echo "===================80%"
+sudo service xrdp start > /dev/null 2>&1
+echo "=======================100%"
+clear
+echo XRDP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "Don't close this tab RDP runs 12 hours"
+echo "👉Keep support akuh.net thank you👈"
+echo "===================================="
+sleep 43210
