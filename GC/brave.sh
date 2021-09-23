9aa916c69dbe551ca11a3f2c60c560d762ae650c
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-23 08:01:58 +0700

    Create brave.sh

---
+++ GC/brave.sh
@@ -0,0 +1,5 @@
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser -y > /dev/null 2>&1
