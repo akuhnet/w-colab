580a81eae2a9b35233ada97cb2c7ac94ae9969e1
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-23 13:37:27 +0700

    Update Lxde.sh

--- GC/Lxde.sh
+++ GC/Lxde.sh
@@ -2,35 +2,32 @@ rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
-echo "================================"
-echo "Wait Downloading file 3-4 minutes"
-echo "More free vps visit 👉akuh.net👈"
-echo "================================"
-sudo apt-get install lxde -y
 clear
 echo "===================================="
-echo "Install xrdp"
+echo "Install Firefox"
 echo "===================================="
-sudo apt install -y xrdp > /dev/null 2>&1
-sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+sudo apt install firefox -y > /dev/null 2>&1
+clear
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+sudo apt install -y xrdp
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh
 sudo service xrdp start > /dev/null 2>&1
 echo "===================================="
-echo "Install Brave Browser"
-echo "===================================="
-sudo apt install apt-transport-https curl > /dev/null 2>&1
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
-sudo apt update > /dev/null 2>&1
-sudo apt install brave-browser -y > /dev/null 2>&1
-echo "===================80%"
-echo "=======================100%"
-echo "===================================="
 echo "Username : root"
-echo "Username : that you made yourself"
+echo "Password : that you made yourself"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "Don't close tab to keep rdp running 50 hours"
-echo "👉Keep support akuh.net thank you👈"
 echo "===================================="
-sleep 9876543210
+echo "Don't close this tab RDP runs 50 hours"
+echo "Keep support akuh.net thank you"
+echo "Wait 1 minute to finish boot"
+echo "===================================="
+echo "===================================="
+docker run -p 3388:3389 dorowu/ubuntu-desktop-lxde-vnc > /dev/null 2>&1
