e3eabd803cb5d8bc2066f8bc0a7f0fd4b63dbd5a
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-23 13:01:07 +0700

    Update Lxde.sh

--- GC/Lxde.sh
+++ GC/Lxde.sh
@@ -1,4 +1,4 @@
-echo -e "$aq\n$aq\n" | passwd
+echo -e "$123\n$123\n" | passwd
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
 chmod +x ng.sh
