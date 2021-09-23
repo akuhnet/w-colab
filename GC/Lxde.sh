59ec9f32a063cd321adb7d1925bf73281235c6c2
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-23 12:05:07 +0700

    Update Lxde.sh

--- GC/Lxde.sh
+++ GC/Lxde.sh
@@ -1,6 +1,6 @@
 echo -e "$123\n$123\n" | sudo passwd
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
@@ -19,7 +19,6 @@ echo "Start RDP"
 echo "===================================="
 sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh
 sudo service xrdp start > /dev/null 2>&1
-usermod -G xrdp root
 echo "===================================="
 echo "Username : root"
 echo "Username : 123"
