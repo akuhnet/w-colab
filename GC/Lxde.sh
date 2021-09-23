7d3d98e12406708a6bdc340b0ce80b14030e6a8f
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-23 14:00:14 +0700

    Update Lxde.sh

--- GC/Lxde.sh
+++ GC/Lxde.sh
@@ -2,32 +2,32 @@ rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://bit.ly/ngrok3389 > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
+echo "================================"
+echo "Wait Downloading file 3-4 minutes"
+echo "More free vps visit 👉akuh.net👈"
+echo "================================"
+sudo apt-get install lxde -y
 clear
 echo "===================================="
-echo "Install Firefox"
+echo "Install xrdp"
 echo "===================================="
-sudo apt install firefox -y > /dev/null 2>&1
-clear
-echo "===================================="
-echo "Install RDP"
-echo "===================================="
-sudo apt install -y xrdp
-clear
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-sudo sed -i.bak '/fi/a ubuntu-desktop-lxde-vnc-session \n' /etc/xrdp/startwm.sh
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
 sudo service xrdp start > /dev/null 2>&1
 echo "===================================="
+echo "Install Brave Browser"
+echo "===================================="
+sudo apt-get update
+sudo apt-get install firefox
+echo "===================80%"
+echo "=======================100%"
+echo "===================================="
 echo "Username : root"
-echo "Password : that you made yourself"
+echo "Username : that you made yourself"
 echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
+echo "Don't close tab to keep rdp running 50 hours"
+echo "👉Keep support akuh.net thank you👈"
 echo "===================================="
-echo "Don't close this tab RDP runs 50 hours"
-echo "Keep support akuh.net thank you"
-echo "Wait 1 minute to finish boot"
-echo "===================================="
-echo "===================================="
-docker run -p 3388:3389 dorowu/ubuntu-desktop-lxde-vnc > /dev/null 2>&1
+sleep 9876543210
