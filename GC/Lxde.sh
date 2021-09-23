fac401384c12802d97be188c1a87b24433117e91
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-23 12:19:10 +0700

    Update Lxde.sh

--- GC/Lxde.sh
+++ GC/Lxde.sh
@@ -1,34 +1,30 @@
 echo -e "$123\n$123\n" | sudo passwd
 rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/akuhnetngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
+echo "================================"
+echo "Wait Downloading file 3-4 minutes"
+echo "More free vps visit 👉akuh.net👈"
+echo "================================"
+sudo apt-get install lxde -y
 clear
+echo "===========40%"
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "===============60%"
 echo "===================================="
 echo "Install Firefox"
 echo "===================================="
 sudo apt install firefox -y > /dev/null 2>&1
 clear
-echo "===================================="
-echo "Install RDP"
-echo "===================================="
-sudo apt install -y xrdp
-clear
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh
+echo "===================80%"
 sudo service xrdp start > /dev/null 2>&1
-echo "===================================="
-echo "Username : root"
-echo "Username : 123"
-echo "RDP Address:"
+echo "=======================100%"
+echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
+echo "Don't close tab colab to keep rdp running 12 hours"
+echo "👉Keep support akuh.net thank you👈"
 echo "===================================="
-echo "Don't close this tab RDP runs 50 hours"
-echo "Keep support akuh.net thank you"
-echo "Wait 1 minute to finish boot"
-echo "===================================="
-echo "===================================="
-docker run -p 3388:3389 -e USER=root -e PASSWORD=123 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
+sleep 9876543210
