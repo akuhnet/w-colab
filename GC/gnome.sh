3eebfd5857a6798bb88aa0eceb8669ed76f5c3d9
akuh <81416541+akuhnet@users.noreply.github.com>
2021-09-26 20:21:47 +0700

    Create gnome.sh

---
+++ GC/gnome.sh
@@ -0,0 +1,27 @@
+echo -e "$123\n$123\n" | sudo passwd
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull frxyt/xrdp:gnome
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : debian"
+echo "Password : ChangeMe"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab RDP runs 50 hours"
+echo "Keep support akuh.net thank you"
+echo "Wait 1 minute to finish bot"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 frxyt/xrdp:gnome > /dev/null 2>&1
