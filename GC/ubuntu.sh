37648fa3b06465383e0f94e282204829153327a9
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-01 10:25:25 +0700

    Update ubuntu.sh

--- GC/ubuntu.sh
+++ GC/ubuntu.sh
@@ -4,6 +4,18 @@ wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3388 &>/dev/null &
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
