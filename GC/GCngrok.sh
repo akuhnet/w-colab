549ffbc1dca60b2e030e07ad7d245fe98531d3af
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-01 10:12:03 +0700

    Update GCngrok.sh

--- GC/GCngrok.sh
+++ GC/GCngrok.sh
@@ -6,4 +6,15 @@ wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.
 unzip ngrok.zip > /dev/null 2>&1
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
-./ngrok tcp --region ap 3388 &>/dev/null &
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo us - United States (Ohio)
+echo eu - Europe (Frankfurt)
+echo ap - Asia/Pacific (Singapore)
+echo au - Australia (Sydney)
+echo sa - South America (Sao Paulo)
+echo jp - Japan (Tokyo)
+echo in - India (Mumbai)
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3388 &>/dev/null &
