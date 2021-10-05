d3ac374d7146f7d9ad007381847c024d9263fcb3
akuh <81416541+akuhnet@users.noreply.github.com>
2021-10-05 07:40:55 +0700

    Update mountdrive.py

--- mountdrive.py
+++ mountdrive.py
@@ -1,2 +1,31 @@
+def MountGDrive():
+    from os import environ as env
+    from google.colab import drive
+
+    config = env['CLOUDSDK_CONFIG']
+    addr = env['TBE_CREDS_ADDR']
+
+    ! runuser -l $user -c "yes | python3 -m pip install --user google-colab"  > /dev/null 2>&1
+
+    mount = """from os import environ as env
 from google.colab import drive
-drive.mount("/content/drive", force_remount=True)
+
+env['CLOUDSDK_CONFIG']  = '{config}'
+env['TBE_CREDS_ADDR'] = '{addr}'
+
+drive.mount('{mountpoint}')""".format(config=config, addr=addr, mountpoint=mountpoint)
+
+    with open('/content/mount.py', 'w') as script:
+        script.write(mount)
+
+    ! runuser -l $user -c "python3 /content/mount.py"
+
+try:
+    mountpoint = f"/home/{username}/drive"
+    user = username
+except NameError:
+    print("username variable not found, mounting at `/content/drive' using `root'")
+    mountpoint = '/content/drive'
+    user = 'root'
+
+MountGDrive()
