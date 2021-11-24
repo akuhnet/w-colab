530af3083c3aa301179191a1abebe969c65d0d2a
akuh <81416541+akuhnet@users.noreply.github.com>
2021-11-24 21:59:45 +0700

    Create goormIDE.sh

---
+++ goormIDE.sh
@@ -0,0 +1,24 @@
+sudo adduser akuhnet --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+echo "akuhnet:123" | sudo chpasswd
+sudo usermod -aG sudo,adm akuhnet
+clear
+echo "===================================="
+echo "Install XFCE"
+echo "===================================="
+sudo apt install -y xfce4 xfce4-goodies > /dev/null 2>&1
+echo "=======25%"
+sudo apt install firefox -y > /dev/null 2>&1
+echo "=============50%"
+sudo apt-get install -y xrdp > /dev/null 2>&1
+sudo apt-get install -y xfce4-terminal > /dev/null 2>&1
+echo "=================70%"
+echo xfce4-session >~/.xsession
+echo "=====================90%"
+sudo service xrdp start
+clear
+echo "=======================100%"
+echo "===================================="
+echo "Username : akuhnet"
+echo "Password : 123"
+echo Go to settings add port 3389
+echo "===================================="
